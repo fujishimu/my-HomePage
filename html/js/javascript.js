29549642026e09fbe1c8dc0921e7e87f0e7f669e
fjsmu <rikugyouza@gmai.com>
2017-01-18 16:45:07 +0900

    大幅更新

--- html/js/javascript.js
+++ html/js/javascript.js
@@ -1,122 +1,104 @@
-//footer要素を書き換え
-$(function() {
-  var leftMargin = window.parent.screen.width * 0.05;
-  $("#footer").css({
-    "left" : leftMargin
-  });
-});
-
-
-// textBox
 $(document).ready(function() {
-  // $("#textBox").fadeIn(1500);
-  $("#textBox").css({opacity: "0"}).animate({
-    "opacity": "1",
-    "marginTop": "20px"
-  }, 1500);
+  $('.animsition').animsition({
+    inClass: 'fade-in-right-sm',
+    outClass: 'fade-out-left',
+    inDuration: 1000,
+    outDuration: 800,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target='_blank']):not([href^='#'])'
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src='loading.svg' />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // 'browser' option allows you to disable the 'animsition' in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the 'animsition' in a browser that does not support 'animation-duration'.
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
 
-  $(".mainContents").hide();
+  $('#human').animate({
+    opacity: '1',
+    left: '150'
+  }, 1000);
 });
-
-
-var leftFlg = true;
-
-// 要素をクリック
-$("#contents .news").click(function() {
-  if(leftFlg) {
-    toLeft(".news");
-    leftFlg = false;
-    $("#textBox").css("cursor", "default");
-  }
-  $(".mainContents.news").show();
-});
-
-$("#contents .about").on('click', function() {
-  if(leftFlg) {
-    toLeft(".about");
-    leftFlg = false;
-    $("#textBox").css("cursor", "default");
-  }
-  $(".mainContents.about").show();
-});
-
-$("#contents .contact").on("click", function() {
-  if(leftFlg) {
-    toLeft(".contact");
-    leftFlg = false;
-    $("#textBox").css("cursor", "default");
-  }
-  $(".mainContents.contact").show();
-});
-
-$("#contents .link").on("click", function() {
-  if(leftFlg) {
-    toLeft(".link");
-    leftFlg = false;
-    $("#textBox").css("cursor", "default");
-  }
-  $(".mainContents.link").show();
-});
-
-// 戻るボタンをクリック
-$(".backBtn").on("click", function() {
-  toLeft("#mainContents");
-  $("#contents h3").css("cursor", "pointer");
-  leftFlg = true;
-  $(".mainContents").fadeOut(2000);
-});
-
-// リストをクリックすると左に消えて、その要素内容が出てくる
-function toLeft(cont) {
-  if(cont == "#mainContents") {  // コンテンツが左へ
-    $("#mainContents").animate({
-      "left": "70vw",
-      opacity: "0"
-    }, 1300, "easeInCubic");
-    $("#mainContents").fadeOut(2000);
-
-    $("#textBox").delay(500);
-    $("#textBox").fadeIn(0);
-    $("#textBox").animate({
-        "left": "60%",
-        opacity: "1"
-    }, 1300, "easeOutCubic");
-  } else{  // メインメニューが左へ
-    $("#textBox").animate({
-      opacity: "0",
-      "left": "100px",
-      "display": "none"
-    }, 1300, "easeOutCubic");
-    $("#textBox").fadeOut(2000);
-
-    $("#mainContents").delay(600);
-    $("#mainContents").fadeIn(0);
-    $("#mainContents").animate({
-        "left": "150px",
-        opacity: "1"
-    }, 1300, "easeOutCubic");
-  }
-}
-
 
 
 // human
+var clickCount = 0;
 var clickFlg = true;
+var angFlg = false;
+const angTimer = 60000;
+const angInterval = 1000;
+$('#human .angry').hide();
 $('#human').click(function() {
+  if(clickCount == 10) {
+    jump();
+    $('#human .angry').fadeIn();
+    angFlg = true;
+    clickCount = 0;
+    setInterval(angry(), angInterval);
+    setTimeout(function() {
+      angFlg = false;
+      $('#human .angry').fadeOut();
+      console.log("flush angry");
+    }, angTimer);
+  }else {
+    if(!angFlg) step();
+  }
+});
+
+$('.top-link').click(function() {
+  $('#human').animate({
+    'left': '-10',
+    opacity: '0'
+  }, 500);
+});
+
+$(function() {
+  if(angFlg) setInterval(angry(), angInterval);
+});
+
+function jump() {
+  $('#human').animate({
+    'top': '300'
+  }, 100);
+  $('#human').animate({
+    'top': '410'
+  },100);
+}
+
+function step() {
   if(clickFlg) {
+    clickCount++;
+    console.log('clickCount: ' + clickCount);
     clickFlg = false;
     var burubruCnt = 4;
     var burubruWidth = 10;
     for(var i = 0; i < burubruCnt; i++) {
-      $("#human").animate({"left": "+=" +burubruWidth}, 100);
+      $('#human').animate({'left': '+=' +burubruWidth}, 70);
       if(i != burubruWidth -1) {
-        $("#human").animate({"left": "-=" + burubruWidth}, 100);
+        $('#human').animate({'left': '-=' + burubruWidth}, 70);
       }else {
-        $("#human").animate({"left": "-=" + burubruWidth},
+        $('#human').animate({'left': '-=' + burubruWidth},
         100, function() {clickFlg = true});
       }
-
       burubruWidth /= 5;
     }
   }
-});
+}
+
+function angry() {
+  console.log("angry");
+  $('#human .angry').animate({
+    opacity: '0'
+  }, 400);
+  $('#human .angry').animate({
+    opacity: '1'
+  }, 400);
+}
